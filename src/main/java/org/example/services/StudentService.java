47103cbc0c8aedf1174c5f68f0419864e2f9daab
Viktor-pnkn <tommegan99@gmail.com>
2023-02-05 21:46:39 +0300

    commit from new branch

---
+++ src/main/java/org/example/services/StudentService.java
@@ -0,0 +1,19 @@
+package org.example.services;
+
+import org.example.models.Student;
+import org.example.repositories.StudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public void addStudent(Student student) {
+        studentRepository.save(student);
+    }
+}
