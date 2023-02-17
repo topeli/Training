93fc2823b05d0534d71fb217e0418f99078904de
Виктория <panasikva@gmail.com>
2023-02-17 19:09:00 +0300

    vika hw 17.02

--- src/main/java/org/example/repositories/TriangleRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repositories;
-
-import org.example.models.Triangle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TriangleRepository extends JpaRepository<Triangle, Long> {
-}
