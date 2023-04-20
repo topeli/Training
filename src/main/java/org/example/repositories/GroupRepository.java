bf5c5115ca24c5a9d78c663dd46a51f4dabd28ac
Viktor-pnkn <tommegan99@gmail.com>
2023-04-20 21:23:12 +0300

    registration

---
+++ src/main/java/org/example/repositories/GroupRepository.java
@@ -0,0 +1,8 @@
+package org.example.repositories;
+
+
+import org.example.models.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, String> {
+}
