f71f88d711eac6dce142ac89bfef93cd414585ac
Viktor-pnkn <tommegan99@gmail.com>
2023-05-12 20:30:01 +0300

    Training

--- src/main/java/org/example/models/Training.java
+++ src/main/java/org/example/models/Training.java
@@ -1,21 +1,35 @@
 package org.example.models;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Date;
+
 @Entity
 @Table(name = "trainings")
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Training {
-    private Long group_id;
     @Id
-    private Long training_id;
-    private Long coach_id;
-    private LocalDateTime start_time;
-    private LocalDateTime end_time;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long trainingId;
 
+    private String classGroup;
+
+    @ManyToOne(fetch = FetchType.EAGER,optional = false)
+    @JoinColumn(name = "coach_id", nullable = false)
+    private Coach coach;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm", timezone = "Europe/Moscow")
+    private Date startTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm", timezone = "Europe/Moscow")
+    private Date endTime;
 }
--- src/main/java/org/example/services/TrainingService.java
+++ src/main/java/org/example/services/TrainingService.java
@@ -1,16 +1,22 @@
 package org.example.services;
+import org.example.models.Coach;
 import org.example.models.Training;
+import org.example.repositories.CoachRepository;
 import org.example.repositories.TrainingRepository;
 import org.springframework.stereotype.Service;
 @Service
 public class TrainingService {
     private final TrainingRepository trainingRepository;
+    private final CoachRepository coachRepository;
 
-    public TrainingService(TrainingRepository trainingRepository) {
+    public TrainingService(TrainingRepository trainingRepository, CoachRepository coachRepository) {
         this.trainingRepository = trainingRepository;
+        this.coachRepository = coachRepository;
     }
 
     public void addTraining(Training training) {
+        Coach coach = coachRepository.findById(1L).orElseThrow();
+        training.setCoach(coach);
         trainingRepository.save(training);
     }
 }
